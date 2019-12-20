dcb5a0481114191ec432e41f5e41c771a0fe6992
Davide Salvetti <davide.salvetti97@live.com>
2019-12-20 13:56:54 +0100

    Ho aggiunto un punto e virgola.

--- ProgettoEconomiaBilancio/src/ProgettoEconomiaBilancio.java
+++ ProgettoEconomiaBilancio/src/ProgettoEconomiaBilancio.java
@@ -35,7 +35,7 @@ public class ProgettoEconomiaBilancio {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);;
 	}
 
 }
